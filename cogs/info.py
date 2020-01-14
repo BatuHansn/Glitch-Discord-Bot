72a17f639393058a80cefa503f4e5aa25b7bc341
Gamefist <ikheetdennis147@gmail.com>
2020-01-14 11:48:28 +0100

    Changed Source Link

--- cogs/info.py
+++ cogs/info.py
@@ -34,7 +34,7 @@ class Information(commands.Cog):
         # Do not remove this command, this has to stay due to the GitHub LICENSE.
         # TL:DR, you have to disclose source according to GNU GPL v3.
         # Reference: https://github.com/AlexFlipnote/discord_bot.py/blob/master/LICENSE
-        await ctx.send(f"**{ctx.bot.user}** is powered by this source code:\nhttps://github.com/AlexFlipnote/discord_bot.py")
+        await ctx.send(f"**{ctx.bot.user}** is powered by this source code:\nhttps://github.com/Gamefist/Reformed-Bot")
 
     @commands.command(aliases=['supportserver', 'feedbackserver'])
     async def botserver(self, ctx):
